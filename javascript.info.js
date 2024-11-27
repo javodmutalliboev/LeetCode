6be74b6de36d11e39aa9ccfa58c9276ccef59811
Javod Mutalliboev <javodmutalliboev@gmail.com>
2024-11-27 23:52:23 +0500

    ifelse read

--- javascript.info.js
+++ javascript.info.js
@@ -64,8 +64,34 @@ let a = prompt("First number?", 1);
 let b = prompt("Second number?", 2);
 
 alert(+a + +b); // 12
-*/
 
-/*
+let name = prompt("What is the official name of JavaScript?", "");
+
+if (name == "ECMAScript") {
+    alert("Right!");
+} else {
+    alert("You don't know? ECMAScript!");
+}
+
+let number = prompt("Enter a number", 0);
+
+if (number > 0) {
+    alert(1);
+} else if (number < 0) {
+    alert(-1);
+} else {
+    alert(0);
+}
+
+let result = a + b < 4 ? "Below" : "Over";
+
+let message =
+login == "Employee"
+? "Hello"
+: login == "Director"
+? "Greetings"
+: login == ""
+? "No login"
+: "";
 
 */
