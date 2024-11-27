77b5c80fbaecf329870b4b3349b50ba5e594ff60
Javod Mutalliboev <javodmutalliboev@gmail.com>
2024-11-27 21:54:13 +0500

    javascript.info is in progress

--- javascript.info.js
+++ javascript.info.js
@@ -5,3 +5,34 @@ alert("Hello");
 /*
 [1, 2].forEach(alert);
 */
+
+// "use strict" should be at the top
+/*
+(function () {
+    "use strict";
+    
+    // ...your code here...
+})();
+*/
+
+/*
+let message;
+message = 'Hello!';
+*/
+
+"use strict";
+
+/*
+let admin;
+let name;
+name = "John";
+admin = name;
+alert(admin);
+
+let ourPlanetName = "Earth";
+let currentUserName = "John";
+
+const BIRTHDAY = "18.04.1982";
+
+const age = someCode(BIRTHDAY);
+*/
