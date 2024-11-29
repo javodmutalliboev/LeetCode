d991fdeb222849e9e5336386c625fc9b35c731af
Javod Mutalliboev <javodmutalliboev@gmail.com>
2024-11-29 13:28:07 +0500

    coding-style read

--- javascript.info.js
+++ javascript.info.js
@@ -219,4 +219,26 @@ ask(
 let num = (1, 2);
 alert(num);
 
+
+
+function pow(x, n) {
+  let result = 1;
+
+  for (let i = 0; i < n; i++) {
+    result *= x;
+  }
+
+  return result;
+}
+
+let x = prompt("x?", "");
+let n = prompt("n?", "");
+
+if (n <= 0) {
+  alert(
+    `Power ${n} is not supported, please enter an integer number greater than zero`
+  );
+} else {
+  alert(pow(x, n));
+}
 */
