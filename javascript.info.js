4a63e7219a936b025474b5cbf8ba615d21d35308
Javod Mutalliboev <javodmutalliboev@gmail.com>
2024-11-29 14:11:30 +0500

    object read

--- javascript.info.js
+++ javascript.info.js
@@ -261,25 +261,98 @@ it("Raises x to the power n", function () {
 });
 */
 
+/*
 describe("Raises x to power n", function () {
-  it("5 in the power of 1 equals 5", function () {
-    assert.equal(pow(5, 1), 5);
-  });
-
+    it("5 in the power of 1 equals 5", function () {
+        assert.equal(pow(5, 1), 5);
+    });
+    
   it("5 in the power of 2 equals 25", function () {
     assert.equal(pow(5, 2), 25);
-  });
-
+});
+  
   it("5 in the power of 3 equals 125", function () {
-    assert.equal(pow(5, 3), 125);
-  });
+      assert.equal(pow(5, 3), 125);
+    });
 });
 
 function pow(x, n) {
-  if (n === 0) {
-    return 1;
-  }
-  return x * pow(x, n - 1);
+    if (n === 0) {
+        return 1;
+    }
+    return x * pow(x, n - 1);
 }
 
 mocha.run();
+*/
+
+// polyfills read
+
+/*
+let user = {};
+user.name = "John";
+user.surname = "Smith";
+user.name = "Pete";
+delete user.name;
+*/
+
+/*
+let schedule = {};
+
+/**
+ * @param {object} obj
+ * @returns {boolean}
+
+function isEmpty(obj) {
+  for (let key in obj) {
+    return false;
+}
+return true;
+}
+
+alert(isEmpty(schedule));
+
+schedule["8:30"] = "get up";
+
+alert(isEmpty(schedule));
+*/
+
+/*
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130,
+};
+
+let sum = 0;
+
+for (let key in salaries) {
+    sum += salaries[key];
+}
+
+alert("Sum: " + sum);
+*/
+
+/*
+let menu = {
+    width: 200,
+    height: 300,
+  title: "My menu",
+};
+
+/**
+ *
+ * @param {object} menu
+ 
+function multiplyNumeric(menu) {
+  for (let key in menu) {
+    if (typeof menu[key] === "number") {
+      menu[key] *= 2;
+    }
+}
+}
+
+multiplyNumeric(menu);
+
+console.log(menu);
+*/
