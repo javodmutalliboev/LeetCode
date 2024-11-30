e45fe5e01f6dbc6226a8291d41db7e826baab36e
Javod Mutalliboev <javodmutalliboev@gmail.com>
2024-11-30 13:23:07 +0500

    object methods read

--- javascript.info.js
+++ javascript.info.js
@@ -356,3 +356,59 @@ multiplyNumeric(menu);
 
 console.log(menu);
 */
+
+/*
+function makeUser() {
+    return {
+        name: "John",
+        ref() {
+            return this;
+        },
+    };
+}
+
+let user = makeUser();
+alert(user.ref().name);
+*/
+
+/*
+let calculator = {
+  read() {
+    this.a = +prompt("a?", "");
+    this.b = +prompt("b?", "");
+  },
+  
+  sum() {
+    return this.a + this.b;
+  },
+  
+  mul() {
+    return this.a * this.b;
+  },
+};
+
+calculator.read();
+alert(calculator.sum());
+alert(calculator.mul());
+*/
+
+/*
+let ladder = {
+  step: 0,
+  up() {
+    this.step++;
+    return this;
+  },
+  down() {
+    this.step--;
+    return this;
+  },
+  showStep: function () {
+    // shows the current step
+    alert(this.step);
+    return this;
+  },
+};
+
+ladder.up().up().down().showStep().down().showStep();
+*/
