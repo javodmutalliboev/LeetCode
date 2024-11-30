29501abe1a1bd4eb220cc128c1fff309849454da
Javod Mutalliboev <javodmutalliboev@gmail.com>
2024-11-30 20:45:36 +0500

    map-set read

--- javascript.info.js
+++ javascript.info.js
@@ -1010,3 +1010,72 @@ for (let num of range) {
   alert(num);
 }
 */
+
+/*
+
+/**
+ *
+ * @param {Array<string>} arr
+ * @returns {Array<string>}
+
+function unique(arr) {
+  // const result = new Set(arr);
+  // result.toString = function () {
+    //   let result = "";
+  //   for (const item of this) {
+  //     result += item + ", ";
+  //   }
+  //   return result;
+  // };
+  return Array.from(new Set(arr));
+}
+
+let values = [
+  "Hare",
+  "Krishna",
+  "Hare",
+  "Krishna",
+  "Krishna",
+  "Krishna",
+  "Hare",
+  "Hare",
+  ":-O",
+];
+
+alert(unique(values)); // Hare, Krishna, :-O
+
+*/
+
+/*
+
+/**
+ *
+ * @param {Array<string>} arr
+ * @returns {Array<string>}
+ 
+function aclean(arr) {
+  let map = new Map();
+
+  for (let word of arr) {
+    let sorted = word.toLowerCase().split("").sort().join("");
+    map.set(sorted, word);
+  }
+
+  return Array.from(map.values());
+}
+
+let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
+
+alert(aclean(arr)); // "nap,teachers,ear" or "PAN,cheaters,era"
+
+*/
+
+let map = new Map();
+
+map.set("name", "John");
+
+let keys = Array.from(map.keys());
+
+keys.push("more");
+
+alert(keys);
