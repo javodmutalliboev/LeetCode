8d53cbf4ba343ba353720bb9bc69af98ca1f6bc4
Javod Mutalliboev <javodmutalliboev@gmail.com>
2024-11-30 20:17:59 +0500

    iterable read

--- javascript.info.js
+++ javascript.info.js
@@ -988,3 +988,25 @@ usersById = {
 }
 
 */
+
+/*
+const range = {
+  from: 1,
+  to: 5,
+  [Symbol.iterator]() {
+    this.current = this.from;
+    return this;
+  },
+  next() {
+    if (this.current <= this.to) {
+      return { done: false, value: this.current++ };
+    } else {
+      return { done: true };
+    }
+  },
+};
+
+for (let num of range) {
+  alert(num);
+}
+*/
